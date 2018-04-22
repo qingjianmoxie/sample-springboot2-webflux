5995a78eef2cbe0fc7918b3b3bb91a51cc258e6e
visakha <viz.ply@gmail.com>
2018-04-21 20:38:39 -0500

    ini commit straight from start.spring.io 2.0.1 Rel

---
+++ src/main/kotlin/org/visakha/springapp/SpringAppApplication.kt
@@ -0,0 +1,11 @@
+package org.visakha.springapp
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringAppApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringAppApplication>(*args)
+}
